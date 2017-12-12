81784ef3ac55842a065a7dd60319e5c96f6c772c
Benjamin Jaeger <benjaminjaeger31@gmail.com>
2017-12-12 18:08:34 +0100

    pet

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    coucou
+</body>
+</html>
 <?php
 /**
  * Created by PhpStorm.
@@ -6,7 +19,11 @@
  * Time: 17:07
  */
 $method = $_SERVER['REQUEST_METHOD'];
-
+?>
+<pre>
+<?= var_dump($method); ?>
+</pre>
+<?php/*
 // Process only when method is POST
 if($method == 'POST'){
     $requestBody = file_get_contents('php://input');
