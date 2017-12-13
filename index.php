2f9eeecc41eac069192e2f5978b53a79aba79457
Benjamin Jaeger <benjaminjaeger31@gmail.com>
2017-12-13 15:24:20 +0100

    comment on fait

--- index.php
+++ index.php
@@ -7,26 +7,32 @@ if($method == 'POST'){
     $json = json_decode($requestBody);
     $text = $json->result->parameters->text;
 
-    switch ($text) {
-        case 'hi':
-            $speech = "Hi, Nice to meet you";
-            break;
+    if (isset($text) && !empty($text)){
+        switch ($text) {
+            case 'hi babe':
+                $speech = "Hi, Nice to meet you";
+                break;
 
-        case 'bye':
-            $speech = "Bye, good night";
-            break;
+            case 'bye bye':
+                $speech = "Bye, good night";
+                break;
 
-        case 'anything':
-            $speech = "Yes, you can type anything here.";
-            break;
+            case 'anything':
+                $speech = "Yes, you can type anything here.";
+                break;
 
-        case 'comment on fait':
-            $speech = "Debrouille toi !!";
-            break;
+            case 'comment on fait':
+                $speech = "Debrouille toi !!";
+                break;
 
-        default:
-            $speech = "Sorry, I didnt get that. Please ask me something else.";
-            break;
+            case 'londres':
+                $speech = getlocation();
+                break;
+
+            default:
+                $speech = "Sorry, I didnt get that. Please ask me something else.";
+                break;
+        }
     }
 
     $response = new \stdClass();
@@ -40,3 +46,18 @@ else
     echo "Method not allowed";
 }
 
+
+
+function getlocation(){
+    echo '<script type="text/javascript">
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(function(position){
+                var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
+                document.getElementById("result").innerHTML = positionInfo;
+            });
+        } else{
+            alert("Sorry, your browser does not support HTML5 geolocation.");
+        }
+        
+    </script>';
+}
