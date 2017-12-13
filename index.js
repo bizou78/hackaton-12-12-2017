f20878677b7d6e93b770aafe0d245a06b60b9f8b
christo9033 <christo9033@hotmail.fr>
2017-12-13 14:27:48 +0100

    add js and html

--- index.js
+++ index.js
@@ -1,102 +1,90 @@
-var accessToken = "0613a18f31eb404e9bdf955d4d4580c3";
-var baseUrl = "https://api.api.ai/v1";
-$(document).ready(function () {
-  $("#input").keypress(function (event) {
-    if (event.which == 13) {
-      event.preventDefault();
-      send();
-      this.value = '';
-    }
-  });
-  $("#rec").click(function (event) {
-    switchRecognition();
-  });
+var accessToken ="c5ec68e1d7ec49868ce55ec2754bf4cd";
+var baseUrl = "https://api.api.ai/v1/";
+$(document).ready(function() {
+    $("#input").keypress(function(event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            send();
+this.value = '';
+        }
+    });
+    $("#rec").click(function(event) {
+        switchRecognition();
+    });
 });
 var recognition;
-
 function startRecognition() {
-  recognition = new webkitSpeechRecognition();
-  recognition.onstart = function (event) {
-    updateRec();
-  };
-  recognition.onresult = function (event) {
-    var text = "";
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      text += event.results[i][0].transcript;
-    }
-    setInput(text);
-    stopRecognition();
-  };
-  recognition.onend = function () {
-    stopRecognition();
-  };
-  recognition.lang = "fr-FR";
-  recognition.start();
+    recognition = new webkitSpeechRecognition();
+    recognition.onstart = function(event) {
+        updateRec();
+    };
+    recognition.onresult = function(event) {
+        var text = "";
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            text += event.results[i][0].transcript;
+        }
+        setInput(text);
+        stopRecognition();
+    };
+    recognition.onend = function() {
+        stopRecognition();
+    };
+    recognition.lang = "fr-FR";
+    recognition.start();
 }
-
 function stopRecognition() {
-  if (recognition) {
-    recognition.stop();
-    recognition = null;
-  }
-  updateRec();
+    if (recognition) {
+        recognition.stop();
+        recognition = null;
+    }
+    updateRec();
 }
-
 function switchRecognition() {
-  if (recognition) {
-    stopRecognition();
-  } else {
-    startRecognition();
-  }
+    if (recognition) {
+        stopRecognition();
+    } else {
+        startRecognition();
+    }
 }
-
 function setInput(text) {
-  $("#input").val(text);
-  send();
+    $("#input").val(text);
+    send();
 }
-
 function updateRec() {
-  $("#rec").text(recognition ? "Stop" : "Parlez");
+    $("#rec").text(recognition ? "Stop" : "Parlez");
 }
-
 function send() {
-  var text = $("#input").val();
-  conversation.push("Moi : " + text + '\r\n');
-  $.ajax({
+    var text = $("#input").val();
+    conversation.push("Moi: " + text + '\r\n');
+    $.ajax({
     type: "POST",
     url: baseUrl + "query?v=20150910",
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     headers: {
-      "Authorization": "Bearer " + accessToken
+    "Authorization": "Bearer " + accessToken
     },
-    data: JSON.stringify({
-      query: text,
-      lang: "en",
-      sessionId: "somerandomthing"
-    }),
-    success: function (data) {
-      var respText = data.result.fulfillment.speech;
-      console.log("Respuesta: " + respText);
-      setResponse(respText);
-      responsiveVoice.speak(respText);
+    data: JSON.stringify({ query: text, lang: "fr", sessionId: "somerandomthing" }),
+    success: function(data) {
+    var respText = data.result.fulfillment.speech;
+    console.log("Respuesta: " + respText);
+    setResponse(respText);
+    responsiveVoice.speak(respText);
     },
-    error: function () {
-      setResponse("Internal Server Error");
+    error: function() {
+    setResponse("Internal Server Error");
     }
-  });
-  //setResponse("Thinking...");
-}
-
-function setResponse(val) {
-  conversation.push("Google : " + val + '\r\n');
-  //$("#response").text(val);
-  $("#response").text(conversation.join(""));
-}
-
+    });
+    //setResponse("Thinking...");
+    }
+    function setResponse(val) {
+    conversation.push("AI: " + val + '\r\n');
+    //$("#response").text(val);
+    $("#response").text(conversation.join(""));
+    }
 function setResponse(val) {
   //Edit "AI: " to change name
-  conversation.push("Google : " + val + '\r\n');
-  $("#response").text(conversation.join(""));
+    conversation.push("Google: " + val + '\r\n');
+    $("#response").text(conversation.join(""));
 }
 var conversation = [];
